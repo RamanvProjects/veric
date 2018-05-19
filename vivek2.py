4660e2394f784741b26894c9187a31c7e6451d30
Eric Rosen <ericrosen@erics-mbp-3.devices.brown.edu>
2018-05-18 20:06:46 -0400

    simple rl converter vivek2

---
+++ vivek2.py
@@ -0,0 +1,29 @@
+def ascii_to_graph(filename="basic.txt"):
+    ascii_file = open(filename, "r")
+    rows = ascii_file.read().split("\n")
+    #print rows
+    graph = {}
+    x = 0
+    y = 0
+    for rx,rv in enumerate(rows):
+        for cy,cv in enumerate(rv):
+            if cv != "w":
+                graph[(rx,cy)] = []
+                if graph[(rx-1,cy)] != "wall":
+                    graph[(rx-1,cy)].append((rx,cy))
+                    graph[(rx,cy)].append((rx-1,cy))
+                if graph[(rx,cy-1)] != "wall":
+                    graph[(rx,cy-1)].append((rx,cy))
+                    graph[(rx,cy)].append((rx,cy-1))
+            else:
+                graph[(rx,cy)] = "wall"
+            y += 1
+        x+=1
+        y=0
+
+    clean_graph = {}
+    for loc in graph.keys():
+        if graph[loc] != "wall":
+            clean_graph[loc] = graph[loc]
+
+    return clean_graph
